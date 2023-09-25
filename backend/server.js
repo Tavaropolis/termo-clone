ca89c1609415d6a7386645e635910a4d7b666dc4
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-09-25 13:20:49 -0300

    errorHandler created

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,12 @@ import 'dotenv/config';
 
 //Importando módulos
 import router from "./routes/Routes.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(router);
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-//   })
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
