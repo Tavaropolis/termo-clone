ef53271f241be34a7abfaa8a5e2f8b66ac6b7fb0
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-09-27 09:51:38 -0300

    Changes in .env

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function openDb() {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/USERBASE");
+        await mongoose.connect(process.env.DB_URL);
     } catch(e) {
         console.log(e);  
     }
