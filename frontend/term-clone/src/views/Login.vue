a3950ebc555f703d5d171712198e18ddcc6af3a3
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-09-27 11:33:36 -0300

    Layout changes

--- frontend/term-clone/src/views/Login.vue
+++ frontend/term-clone/src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-container flex flex-col items-center justify-center">
-    <div class="login-box rounded flex flex-col items-center justify-evenly">
+    <div class="login-box rounded flex flex-col items-center justify-center gap-y-4">
       <h1>Login</h1>
       <form action="" method="post">
         <div class="input-container flex flex-col items-center">
@@ -9,6 +9,10 @@
         </div>
         <button type="submit" class="rounded">Logar</button>
       </form>
+      <div class="secondary-buttons w-60 flex flex-row justify-between">
+        <button type="button" class="rounded">Criar conta</button>
+        <button type="button" class="rounded">Esqueci senha</button>
+      </div>
     </div>
   </div>
 </template>
@@ -64,10 +68,18 @@ input:focus {
   color: var(--vt-c-white);
 }
 
-button {
+button[type="submit"] {
   background-color: #EDA571;
   width: 12vw;
-  height: 35px;
+  height: 40px;
+  transition: all 1s ease-in-out;
+}
+
+button[type="button"] {
+  background-color: #EDA571;
+  width: 9vw;
+  height: 20px;
+  font-size: 12px;
   transition: all 1s ease-in-out;
 }
 
