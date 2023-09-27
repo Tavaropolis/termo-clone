6882656cbe762263db6362c0c1b782a1bbe34da5
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-09-27 09:31:40 -0300

    Modifing routes

--- backend/routes/Routes.js
+++ backend/routes/Routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
+import User from "../models/userModel.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({
-        message: "API listening"
-    })
+router.get('/', async (req, res) => {
+    try {
+        const usuarios = await User.find({});
+        res.json(usuarios);
+    } catch(e) {
+        console.log(e);
+    }
 })
 
 export default router;
