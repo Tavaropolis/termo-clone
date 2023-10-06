6a22dd52217c1c7a218601268d75bd98b9642e90
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-10-06 08:16:40 -0300

    Cors apllied

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 //Importando bibliotecas
 import express from "express";
 import 'dotenv/config';
+import cors from "cors";
 
 //Importando módulos
 import router from "./routes/Routes.js";
@@ -9,7 +10,13 @@ import openDb from "./db.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const corsOption = {
+  origin: 'http://127.0.0.1:5173'
+}
+app.use(cors(corsOption));
 app.use(router);
+
 openDb();
 
 app.listen(port, () => {
