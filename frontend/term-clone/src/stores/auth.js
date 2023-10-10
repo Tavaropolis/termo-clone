4908855f0af4204bd5e97e48479cac61fff10e14
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-10-10 13:00:43 -0300

    Typescript artefacts cleaned

--- frontend/term-clone/src/stores/auth.js
+++
@@ -1,7 +0,0 @@
-import { ref } from 'vue';
-import { defineStore } from 'pinia';
-export const authUser = defineStore('authUser', () => {
-    const token = ref();
-    return { token };
-});
-//# sourceMappingURL=auth.js.map
