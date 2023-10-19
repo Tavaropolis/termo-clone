500adadd23ff40d1ff1cd93e8db5d976c4f5ab23
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-10-19 10:29:57 -0300

    PasswordStrength component added

---
+++ frontend/term-clone/src/components/PasswordStrength.vue
@@ -0,0 +1,50 @@
+<template>
+ <div class="password-strength-container h-full w-9/12 flex items-center justify-center">
+    <div class="box-result h-4 w-1/3 bg-red-500 rounded"></div>
+    <div class="box-result h-4 w-1/3 bg-white rounded"></div>
+    <div class="box-result h-4 w-1/3 bg-white rounded"></div>
+    <span class="font-semibold ml-2">{{ strengthMsg }}</span>
+ </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    strength: {
+        type: String,
+        required: true
+    }
+});
+
+const strengthMsg = computed(() => {
+   let boxResults: NodeListOf<Element> = document.querySelectorAll(".box-result");
+   console.log(boxResults);
+   if(props.strength == "weak") {
+      boxResults[1].classList.remove("bg-yellow-500");
+      boxResults[2].classList.remove("bg-green-500");
+      boxResults[1].classList.add("bg-white");
+      boxResults[2].classList.add("bg-white");
+
+      return "Fraco";
+   } else if(props.strength == "medium") {
+      boxResults[1].classList.add("bg-yellow-500");
+      boxResults[2].classList.remove("bg-green-500");
+      boxResults[2].classList.add("bg-white");
+
+      return "Médio";
+   } else if(props.strength == "strong") {
+      boxResults[2].classList.add("bg-green-500");
+      boxResults[2].classList.remove("bg-white");
+
+      return "Forte";
+   } else {
+      return "";
+   }
+})
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style.scss";
+
+</style>
--- frontend/term-clone/src/views/login/NewAccount.vue
+++ frontend/term-clone/src/views/login/NewAccount.vue
@@ -5,27 +5,79 @@
       <form action="" method="post">
         <div class="input-container flex flex-col items-center">
           <div class="flex items-center justify-center">
-            <Icon icon="fa6-regular:user" class="icon-account"/>
-            <input type="text" placeholder="Usuário" class="rounded"/>
+            <Icon icon="fa6-regular:user" class="icon-account" />
+            <input
+              @focus="backGroundChange"
+              @focusout="backGroundChange"
+              required
+              type="text"
+              placeholder="Usuário"
+              class="rounded"
+            />
           </div>
           <div class="flex items-center justify-center">
-            <Icon v-if="isPasswordVisible" @click="showPassword" icon="fa6-regular:eye-slash" class="icon-account password-icon"/>
-            <Icon v-else @click="showPassword" icon="fa6-regular:eye" class="icon-account password-icon"/>
-            <input type="password" placeholder="Senha" class="rounded" id="passwordInput"/>
+            <Icon
+              v-if="isPasswordVisible"
+              @click="showPassword"
+              icon="fa6-regular:eye-slash"
+              class="icon-account password-icon"
+            />
+            <Icon
+              v-else
+              @click="showPassword"
+              icon="fa6-regular:eye"
+              class="icon-account password-icon"
+            />
+            <input
+              v-model="passwordInput"
+              @focus="backGroundChange"
+              @focusout="backGroundChange"
+              required
+              type="password"
+              placeholder="Senha"
+              class="rounded"
+              id="passwordInput"
+            />
+          </div>
+          <PasswordStrength v-show="passwordInput" :strength="passwordEntropy"/>
+          <div class="flex items-center justify-center">
+            <Icon
+              v-if="isConfirmVisible"
+              @click="showConfirm"
+              icon="fa6-regular:eye-slash"
+              class="icon-account password-icon"
+            />
+            <Icon
+              v-else
+              @click="showConfirm"
+              icon="fa6-regular:eye"
+              class="icon-account password-icon"
+            />
+            <input
+              @focus="backGroundChange"
+              @focusout="backGroundChange"
+              required
+              type="password"
+              placeholder="Confirmar senha"
+              class="rounded"
+              id="confirmInput"
+            />
           </div>
           <div class="flex items-center justify-center">
-              <Icon v-if="isConfirmVisible" @click="showConfirm" icon="fa6-regular:eye-slash" class="icon-account password-icon"/>
-              <Icon v-else @click="showConfirm" icon="fa6-regular:eye" class="icon-account password-icon"/>
-              <input type="password" placeholder="Confirmar senha" class="rounded" id="confirmInput" />
-          </div>
-          <div class="flex items-center justify-center">
-              <Icon icon="mdi:at" class="icon-account"/>
-              <input type="email" placeholder="Email" class="rounded" />
+            <Icon icon="mdi:at" class="icon-account" />
+            <input
+              @focus="backGroundChange"
+              @focusout="backGroundChange"
+              required
+              type="email"
+              placeholder="Email"
+              class="rounded"
+            />
           </div>
         </div>
         <div class="group-buttons w-1/2 flex flex-row justify-around">
           <button type="reset" class="rounded">Limpar</button>
-          <button type="submit" class="rounded">Criar conta</button>
+          <button @click="formReq" type="submit" class="rounded">Criar conta</button>
         </div>
       </form>
     </div>
@@ -33,29 +85,91 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { Icon } from '@iconify/vue'
 
-const isPasswordVisible = ref<boolean>(false);
-const isConfirmVisible = ref<boolean>(false);
+import PasswordStrength from "@/components/PasswordStrength.vue"
+
+const passwordInput = ref<string>("");
+const isPasswordVisible = ref<boolean>(false)
+const isConfirmVisible = ref<boolean>(false)
+
+const formReq = (e: Event) => {
+  e.preventDefault();
+}
+
+const passwordEntropy = computed(() => {
+  const upperCaseLetters = 26;
+  const lowerCaseLetters = 26;
+  const numbersChar = 10;
+  const specialChar = 32;
+
+  const passwordLength = passwordInput.value.length;
+  
+  let diversity = 0;
+
+  if(/[A-Z]/.test(passwordInput.value)) {
+    diversity += upperCaseLetters;
+  }
+
+  if(/[a-z]/.test(passwordInput.value)) {
+    diversity += lowerCaseLetters;
+  }
+
+  if(/[\d]/.test(passwordInput.value)) {
+    diversity += numbersChar;
+  }
+
+  if(/[!@#$%^&*()_+]/.test(passwordInput.value)) {
+    diversity += specialChar;
+  }
+
+  const entropy = passwordLength * (Math.log2(diversity));
+
+  if(entropy <= 40) {
+    console.log(`Senha fraca: ${entropy}`);
+    return "weak";
+  } else if (entropy <= 64) {
+    console.log(`Senha média: ${entropy}`);
+    return "medium";
+  } else if (entropy > 64 ) {
+    console.log(`Senha forte: ${entropy}`);
+    return "strong";
+  } else {
+    return "";
+  }
+});
 
 //Funções de estilização
+const backGroundChange = () => {
+  const body = document.querySelector('.account-container')
+
+  body?.classList.toggle('dark-background')
+}
+
 const showPassword = () => {
-    let passwordInput: HTMLElement | null = document.getElementById("passwordInput");
-    !isPasswordVisible.value ? passwordInput?.setAttribute("type", "text") : passwordInput?.setAttribute("type", "password");
-    isPasswordVisible.value = !isPasswordVisible.value;
+  let passwordInputField: HTMLElement | null = document.getElementById('passwordInput')
+  !isPasswordVisible.value
+    ? passwordInputField?.setAttribute('type', 'text')
+    : passwordInputField?.setAttribute('type', 'password')
+  isPasswordVisible.value = !isPasswordVisible.value
 }
 
 const showConfirm = () => {
-    let confirmInput: HTMLElement | null = document.getElementById("confirmInput");
-    !isConfirmVisible.value ? confirmInput?.setAttribute("type", "text") : confirmInput?.setAttribute("type", "password");
-    isConfirmVisible.value = !isConfirmVisible.value;
+  let confirmInputField: HTMLElement | null = document.getElementById('confirmInput')
+  !isConfirmVisible.value
+    ? confirmInputField?.setAttribute('type', 'text')
+    : confirmInputField?.setAttribute('type', 'password')
+  isConfirmVisible.value = !isConfirmVisible.value
 }
 </script>
 
 <style lang="scss" scoped>
-@import "../../assets/style.scss";
+@import '../../assets/style.scss';
 
+.account-container {
+  transition: all 1s ease-in-out;
+}
 .account-box {
   background-color: $secondary-color;
   width: 50vw;
