7e3c942c5bacd4ede46d0ac6f248adfd593978b9
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-10-20 10:13:30 -0300

    InputAlert.vue created

---
+++ frontend/term-clone/src/components/InputAlert.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="input-alert-container h-full w-[25vw] ml-[3vw] flex justify-end border-b-2 border-red-500"
+  >
+    <span class="font-semibold text-red-500">{{ alertMsg }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  alertMsg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style.scss';
+</style>
