fa51bbbb324a56398ae762b355b31af6a1d7627a
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-10-23 11:36:11 -0300

    Transition in InputAlert.vue

--- frontend/term-clone/src/components/InputAlert.vue
+++ frontend/term-clone/src/components/InputAlert.vue
@@ -1,9 +1,11 @@
 <template>
-  <div
-    class="input-alert-container h-full w-[25vw] ml-[3vw] flex justify-end border-b-2 border-red-500"
-  >
-    <span class="font-semibold text-red-500">{{ alertMsg }}</span>
-  </div>
+  <Transition name="show-alert">
+    <div
+      class="input-alert-container h-full w-[25vw] ml-[3vw] flex justify-end border-b-2 border-red-500"
+    >
+      <span class="font-semibold truncate text-red-500">{{ alertMsg }}</span>
+    </div>
+  </Transition>
 </template>
 
 <script lang="ts" setup>
@@ -17,4 +19,24 @@ const props = defineProps({
 
 <style lang="scss" scoped>
 @import '../assets/style.scss';
+
+.show-alert-enter-active {
+  animation: show-alert 1.2s linear;
+}
+.show-alert-leave-active {
+  animation: show-alert 1.2s reverse;
+}
+
+@keyframes show-alert {
+  0% {
+    opacity: 0;
+    width: 0vw;
+    align-self: flex-end;
+  }
+  100% {
+    opacity: 1;
+    width: 25vw;
+    align-self: flex-end;
+  }
+}
 </style>
