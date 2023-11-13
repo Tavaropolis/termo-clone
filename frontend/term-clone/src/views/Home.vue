b0bd03314570be56b2b20ac19a957afcdf01d4f8
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-11-13 13:49:32 -0300

    Word check implementation

--- frontend/term-clone/src/views/Home.vue
+++ frontend/term-clone/src/views/Home.vue
@@ -19,7 +19,8 @@
         </button>
       </div>
     </header>
-    <main class="flex justify-between mt-16">
+    <ContentChip :message="chipMessage" @closeChip="chipActive = false" class="invisible" id="contentchip"/>
+    <main class="flex justify-between mt-3">
       <div ref="mainDiv" class="main-content grid grid-cols-5 grid-rows-6 w-[22vw] h-[54vh]">
         <input
           v-for="index in 5"
@@ -28,7 +29,8 @@
           @input="inputValidation(index - 1)"
           @keyup.right="focusNextElement(index - 1)"
           @keyup.left="focusPreviousElement(index - 1)"
-          @keyup.enter="confirmTry(inputRows[0])"
+          @keyup.delete="deletePrevious(index - 1)"
+          @keyup.enter="confirmTry()"
           maxlength="1"
           type="text"
           class="rounded main-input input-row-0"
@@ -41,7 +43,8 @@
           @input="inputValidation(index - 1)"
           @keyup.right="focusNextElement(index - 1)"
           @keyup.left="focusPreviousElement(index - 1)"
-          @keyup.enter="confirmTry(inputRows[1])"
+          @keyup.delete="deletePrevious(index - 1)"
+          @keyup.enter="confirmTry"
           maxlength="1"
           type="text"
           class="rounded main-input input-row-1"
@@ -54,7 +57,8 @@
           @input="inputValidation(index - 1)"
           @keyup.right="focusNextElement(index - 1)"
           @keyup.left="focusPreviousElement(index - 1)"
-          @keyup.enter="confirmTry(inputRows[2])"
+          @keyup.delete="deletePrevious(index - 1)"
+          @keyup.enter="confirmTry"
           maxlength="1"
           type="text"
           class="rounded main-input input-row-2"
@@ -67,7 +71,8 @@
           @input="inputValidation(index - 1)"
           @keyup.right="focusNextElement(index - 1)"
           @keyup.left="focusPreviousElement(index - 1)"
-          @keyup.enter="confirmTry(inputRows[3])"
+          @keyup.delete="deletePrevious(index - 1)"
+          @keyup.enter="confirmTry"
           maxlength="1"
           type="text"
           class="rounded main-input input-row-3"
@@ -80,7 +85,8 @@
           @input="inputValidation(index - 1)"
           @keyup.right="focusNextElement(index - 1)"
           @keyup.left="focusPreviousElement(index - 1)"
-          @keyup.enter="confirmTry(inputRows[4])"
+          @keyup.delete="deletePrevious(index - 1)"
+          @keyup.enter="confirmTry()"
           maxlength="1"
           type="text"
           class="rounded main-input input-row-4"
@@ -93,7 +99,8 @@
           @input="inputValidation(index - 1)"
           @keyup.right="focusNextElement(index - 1)"
           @keyup.left="focusPreviousElement(index - 1)"
-          @keyup.enter="confirmTry(inputRows[5])"
+          @keyup.delete="deletePrevious(index - 1)"
+          @keyup.enter="confirmTry()"
           maxlength="1"
           type="text"
           class="rounded main-input input-row-5"
@@ -102,23 +109,23 @@
     </main>
     <div class="letter-row mt-3 w-[55vw]">
       <div class="letter-row-1 flex">
-        <button v-for="(letter, index) in firstLetterRow" :key="letter" @click="buttonInput(index, 1)" class="letter-row-button rounded">
+        <button v-for="(letter, index) in firstLetterRow" :key="letter" @click="buttonInput(index, 1)" class="letter-row-button-0 rounded">
           {{ letter }}
         </button>
       </div>
       <div class="letter-row-2 flex ml-4">
-        <button v-for="(letter, index) in secondLetterRow" :key="letter" @click="buttonInput(index, 2)" class="letter-row-button rounded">
+        <button v-for="(letter, index) in secondLetterRow" :key="letter" @click="buttonInput(index, 2)" class="letter-row-button-1 rounded">
           {{ letter }}
         </button>
-        <button id="backspace-button" class="rounded flex justify-center items-center">
+        <button @click="deletePreviousButton" id="backspace-button" class="rounded flex justify-center items-center">
           <Icon icon="material-symbols:backspace-outline" class="w-7 h-7" />
         </button>
       </div>
       <div class="letter-row-3 flex ml-8">
-        <button v-for="(letter, index) in thirdLetterRow" :key="letter" @click="buttonInput(index, 3)" class="letter-row-button rounded">
+        <button v-for="(letter, index) in thirdLetterRow" :key="letter" @click="buttonInput(index, 3)" class="letter-row-button-2 rounded">
           {{ letter }}
         </button>
-        <button @click="confirmTry(inputRows[mainRow])" id="enter-button" class="rounded">ENTER</button>
+        <button @click="confirmTry" id="enter-button" class="rounded">ENTER</button>
       </div>
     </div>
   </div>
@@ -128,6 +135,9 @@ import { ref, onMounted, watch } from 'vue'
 import { onClickOutside } from '@vueuse/core'
 import { Icon } from '@iconify/vue'
 import axios from 'axios'
+import { Word } from '@andsfonseca/palavras-pt-br'
+
+import ContentChip from "../components/ContentChip.vue";
 
 //Elementos de referência
 const mainDiv = ref(null);
@@ -135,6 +145,7 @@ const mainDiv = ref(null);
 //Variáveis
 const mainWord = ref<string[]>([])
 const mainRow = ref<number>(0)
+const endGame = ref<boolean>(false)
 const isRowFull = ref<boolean>(false);
 const inputRows = ref<string[][]>([
   [], [], [], [], [], []
@@ -144,9 +155,13 @@ const firstLetterRow = ref<string[]>(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o
 const secondLetterRow = ref<string[]>(['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'])
 const thirdLetterRow = ref<string[]>(['z', 'x', 'c', 'v', 'b', 'n', 'm'])
 
+const chipActive = ref<boolean>(false);
+const chipMessage = ref<string>("");
+
 onMounted(() => {
   getWord()
   focusNextLine()
+  getData()
 })
 
 //Recupera uma palavra aleatória do banco de dados
@@ -164,7 +179,7 @@ const getWord = async () => {
 
 const inputValidation = (index: number) => {
   const activeRow = document.querySelectorAll(`.input-row-${mainRow.value}`)
-  if (/[a-zA-Z]/.test(inputRows.value[mainRow.value][index])) {
+  if (/[a-zA-ZÇç]/.test(inputRows.value[mainRow.value][index])) {
     
     (activeRow[index] as HTMLInputElement).classList.add('input-animation');
     (activeRow[index + 1] as HTMLInputElement).setSelectionRange(0, 1);
@@ -179,12 +194,15 @@ const buttonInput = ((index: number, letterRow: number) => {
     if (!inputRows.value[mainRow.value][i]) {
       if(letterRow == 1) {
         inputRows.value[mainRow.value][i] = firstLetterRow.value[index];
+        focusNextElement(i);
         return
       } else if(letterRow == 2) {
         inputRows.value[mainRow.value][i] = secondLetterRow.value[index];
+        focusNextElement(i);
         return
       }else if (letterRow == 3) {
         inputRows.value[mainRow.value][i] = thirdLetterRow.value[index];
+        focusNextElement(i);
         return
       }
     }
@@ -192,25 +210,60 @@ const buttonInput = ((index: number, letterRow: number) => {
 });
 
 //Funções de quando o usuário aperta enter e confirma a tentativa
-const confirmTry = ((row: any) => {
-  rowValidation(row);
-  if (isRowFull.value) {
+const confirmTry = (() => {
+  wordValidation();
+
+  if (isRowFull.value && !chipActive.value) {
       checkWordResult();
+      checkVictory();
       endLineAnimation();
+      saveData();
       mainRow.value += 1
       isRowFull.value = false
-      return
     }
 })
 
+const wordValidation = (() => {
+  const activeRow = document.querySelectorAll(`.input-row-${mainRow.value}`);
+  let userWord = [];
 
-const rowValidation = ((row: any) => {
-  for(let i = 0; i < 5; i++) {
-    if(!row[i]) {
-      return;
-    }
+  for(let value of inputRows.value[mainRow.value]){
+    value = value.toLowerCase();
+    userWord.push(value);
   }
-  isRowFull.value = true;
+
+  if(userWord.length < 5) {
+    isRowFull.value = false
+    chipActive.value = true;
+    chipMessage.value = "Só palavras com 5 letras"
+
+    for(let row of activeRow) {
+      if(row.classList.contains("wrong-animation")) {
+        row.classList.remove("wrong-animation");
+      }
+
+      (row as HTMLInputElement).offsetWidth; //Reseta a animação
+      row.classList.add("wrong-animation");
+    }
+
+    return
+  }
+
+  chipActive.value = (!Word.checkValid(userWord.join("")))
+  if(chipActive.value) {
+    chipMessage.value = "Esta palavra não é aceita"
+
+    for(let row of activeRow) {
+      if(row.classList.contains("wrong-animation")) {
+        row.classList.remove("wrong-animation");
+      }
+
+      (row as HTMLInputElement).offsetWidth; //Reseta a animação
+      row.classList.add("wrong-animation");
+    }
+  } else {
+    isRowFull.value = true;
+  }
 })
 
 const checkWordResult = (() => {
@@ -235,6 +288,35 @@ const checkWordResult = (() => {
     }
   }
 });
+
+const checkVictory = (() => {
+  const activeRow = document.querySelectorAll(`.input-row-${mainRow.value}`);
+
+  for(let element of activeRow) {
+    if(!element.classList.contains("success")) {
+      return
+    }
+  }
+
+  chipActive.value = true
+  chipMessage.value = "Impressionante"
+  endGame.value = true
+})
+
+const saveData = (() => {
+  let inputRowsData = Object.assign({}, inputRows.value);
+  localStorage.setItem("inputRows", JSON.stringify(inputRowsData));
+  
+})
+
+const getData = (() => {
+  let inputStorage = localStorage.getItem("inputRows")
+
+  if(inputStorage) {
+    let inputStorageJSON = JSON.parse(inputStorage!);
+    inputRows.value = Object.values(inputStorageJSON)
+  }
+})
 
 //Foca o primeiro elemento da linha de baixo
 const focusNextLine = () => {
@@ -270,17 +352,57 @@ const focusPreviousElement = (index: number) => {
   ;(activeRow[index - 1] as HTMLInputElement).focus()
 }
 
+const deletePrevious = ((index: number) => {
+  if(index == 0) {
+    return
+  }
+
+  if(!inputRows.value[mainRow.value][index]) {
+    inputRows.value[mainRow.value][index - 1] = "";
+    focusPreviousElement(index);
+  }
+});
+
+const deletePreviousButton = (() => {
+  const activeRow = document.querySelectorAll(`.input-row-${mainRow.value}`)
+
+  for(let i = 0; i < 5; i++) {
+    if(activeRow[i] === document.activeElement) {
+      if(i == 0) {
+        return
+      }
+      
+      if(!inputRows.value[mainRow.value][i]) {
+        inputRows.value[mainRow.value][i - 1] = "";
+        focusPreviousElement(i);
+        return
+      }
+  }
+}})
+//Foca primeiro input vazio quando o usuário clica fora
+onClickOutside(mainDiv, () => {
+  const activeRow = document.querySelectorAll(`.input-row-${mainRow.value}`)
+    for (let i = 0; i < 5; i++) {
+      if (!inputRows.value[mainRow.value][i]) {
+        (activeRow[i] as HTMLInputElement).focus()
+        return
+      } 
+
+    (activeRow[0] as HTMLInputElement).focus()
+  }
+})
+
 watch(mainRow, () => {
   const activeRow = document.querySelectorAll(`.input-row-${mainRow.value}`)
   const previousRow = document.querySelectorAll(`.input-row-${mainRow.value - 1}`)
 
-  activeRow.forEach((item) => {
-    item.removeAttribute('disabled')
-  })
+  for(let row of activeRow) {
+    row.removeAttribute('disabled');
+  }
 
-  previousRow.forEach((item) => {
-    item.setAttribute('disabled', 'true');
-  })
+  for(let row of previousRow) {
+    row.setAttribute('disabled', 'true');
+  }
 
   if (mainRow.value == 7) {
     return
@@ -289,24 +411,56 @@ watch(mainRow, () => {
   focusNextLine()
 })
 
-//Foca primeiro input vazio quando o usuário clica fora
-onClickOutside(mainDiv, () => {
-  const activeRow = document.querySelectorAll(`.input-row-${mainRow.value}`)
+watch(chipActive, () => {
+  const contentchip = document.getElementById("contentchip");
 
-  for (let i = 0; i < 5; i++) {
-    if (!inputRows.value[mainRow.value][i]) {
-      (activeRow[i] as HTMLInputElement).focus()
-      return
+  if(chipActive.value == true) {
+    contentchip?.classList.remove("invisible");
+  } else {
+    contentchip?.classList.add("invisible");
+  }
+})
+
+watch(endGame, () => {
+  if(endGame.value) {
+    for(let i = 0; i < 6; i++) {
+      const activeRow = document.querySelectorAll(`.input-row-${i}`);
+      for(let j = 0; j < 5; j++) {
+        activeRow[j].setAttribute('disabled', 'true');
+      }
     }
+
+    for(let i = 0; i < 3; i++) {
+      const letterRow = document.querySelectorAll(`.letter-row-button-${i}`);
+      for(let letter of letterRow) {
+        letter.setAttribute("disabled", "true");
+      }
+    }
+
+    const enterButton = document.getElementById("enter-button");
+    enterButton?.setAttribute("disabled", "true");
+
+    const backspaceButton = document.getElementById("backspace-button");
+    backspaceButton?.setAttribute("disabled", "true")
   }
 })
 
 //Funções de estilização
 const endLineAnimation = () => {
   const activeRow = document.querySelectorAll(`.input-row-${mainRow.value}`);
+
+  for(let row of activeRow) {
+    if((row as HTMLInputElement).classList.contains("wrong-animation")) {
+      row.classList.remove("wrong-animation");
+    }
+    if((row as HTMLInputElement).classList.contains("input-animation")) {
+      row.classList.remove("input-animation");  
+    }
+  }
+
   activeRow[0].classList.add("flip-animation"); 
   if(activeRow[0].classList.contains("success")) { 
-    activeRow[0].classList.add("letter-sucess"); 
+    activeRow[0].classList.add("letter-success"); 
   } else if (activeRow[0].classList.contains("warn")) {
     activeRow[0].classList.add("flip-animation");   
     activeRow[0].classList.add("letter-warn");  
@@ -318,7 +472,7 @@ const endLineAnimation = () => {
     activeRow[i].addEventListener("animationend", ()=> {
       if(activeRow[i+1].classList.contains("success")) {
         activeRow[i+1].classList.add("flip-animation");   
-        activeRow[i+1].classList.add("letter-sucess");   
+        activeRow[i+1].classList.add("letter-success");   
       } else if(activeRow[i+1].classList.contains("warn")) {
         activeRow[i+1].classList.add("flip-animation");   
         activeRow[i+1].classList.add("letter-warn");        
@@ -328,7 +482,27 @@ const endLineAnimation = () => {
       }
     })
   }
+  buttonColorization();
 }
+
+const buttonColorization = (() => {
+  const activeRow = document.querySelectorAll(`.input-row-${mainRow.value}`);
+
+  for(let row of activeRow) {
+    for(let i = 0; i < 3; i++) {
+      const letterRow = document.querySelectorAll(`.letter-row-button-${i}`);
+      for(let letter of letterRow) {
+      if((row as HTMLInputElement).value == letter.textContent) {
+        if(row.classList.contains("success")) {
+          letter.classList.add("letter-success")
+        } else if(row.classList.contains("warn")) {
+          letter.classList.add("letter-warn")
+        }
+      }
+    }
+  }
+  }
+})
 
 </script>
 <style lang="scss" scoped>
@@ -371,7 +545,7 @@ const endLineAnimation = () => {
   background-color: $aswered-color;
 }
 
-.letter-row-button {
+.letter-row-button-0, .letter-row-button-1, .letter-row-button-2 {
   width: 60px;
   height: 45px;
   background-color: $focus-color;
@@ -400,20 +574,38 @@ const endLineAnimation = () => {
   margin-left: 30px;
 }
 
+.letter-row-button-0:disabled, .letter-row-button-1:disabled, .letter-row-button-2:disabled, 
+#enter-button:disabled, #backspace-button:disabled  {
+  opacity: 0.4;
+}
+
 .input-animation {
   animation-duration: 0.2s;
   animation-name: input-animation;
 }
 
 .flip-animation {
-  animation-duration: 0.4s;
+  animation-duration: 0.5s;
   animation-name: flip-animation;
 }
 
-.main-content .letter-sucess {
-  background-color: $sucess-color
+.wrong-animation {
+  animation-duration: 0.3s;
+  animation-name: wrong-animation;
+}
+
+.main-content .letter-success {
+  background-color: $success-color
 }
 .main-content .letter-warn {
+  background-color: $attention-color
+}
+
+.letter-row-button-0.letter-success, .letter-row-button-1.letter-success, .letter-row-button-2.letter-success {
+  background-color: $success-color
+}
+
+.letter-row-button-0.letter-warn, .letter-row-button-1.letter-warn, .letter-row-button-2.letter-warn {
   background-color: $attention-color
 }
 
@@ -433,6 +625,25 @@ const endLineAnimation = () => {
   100% {
     transform: rotateY(0deg);
   }
+}
+
+@keyframes wrong-animation {
+  20% {
+    transform: translateX(-20px);
+  } 
+  40% {
+    transform: translateX(20px);
+  }
+  60% {
+    transform: translateX(-10px);
+  }
+  80% {
+    transform: translateX(10px);
+  }
+  100% {
+    transform: translateX(0px);
+  } 
+
 }
 
 </style>
