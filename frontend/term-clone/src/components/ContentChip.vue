0ea20b2e4e96d5bed579e8b3bc3fbbbc9b5c2694
Tavaropolis <tavares.ag.gabriel@gmail.com>
2023-11-13 13:49:51 -0300

    ContentChip.vue created

---
+++ frontend/term-clone/src/components/ContentChip.vue
@@ -0,0 +1,32 @@
+<template>
+    <div ref="chip" class="content-chip flex flex-col items-center justify-center rounded">
+        <p>{{ props.message }}</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { onClickOutside } from '@vueuse/core'
+
+const chip = ref(null);
+const props = defineProps({
+    message: {
+        type: String,
+        required: true
+    }
+})
+const emit = defineEmits(['closeChip']);
+
+onClickOutside(chip, () => emit('closeChip'))
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style.scss';
+.content-chip {
+    width: 20vw;
+    height: 5vh;
+    background-color: $chip-color;
+    margin: 10px;
+}
+
+</style>
